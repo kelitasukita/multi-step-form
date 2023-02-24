519f6df2504fcec4544ab2ca77a8ff9b2503e432
Kelita Lamar <kelitasukita@gmail.com>
2023-02-24 22:29:00 +0100

    Create Menu Item component

---
+++ src/components/MenuItem/index.tsx
@@ -0,0 +1,17 @@
+import './styles.scss';
+
+export default function menuItem() {
+  return (
+    <div className="container">
+      <div className="step1">
+        <div className="firstStep">
+          <div className="menu-ball">
+            1
+          </div>
+          <span> STEP 1</span>
+          <strong>YOUR INFO</strong>
+        </div>
+      </div>
+    </div>
+  )
+}
