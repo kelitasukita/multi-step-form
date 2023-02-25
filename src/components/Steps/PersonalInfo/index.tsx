4460a88a75a2fbfe94c536203dc28dbf50e7b88f
Kelita Lamar <kelitasukita@gmail.com>
2023-02-25 23:41:49 +0100

    Add form with name, email and phone number

--- src/components/Steps/PersonalInfo/index.tsx
+++ src/components/Steps/PersonalInfo/index.tsx
@@ -3,8 +3,25 @@ import './index.scss';
 export default function personalInfo() {
   return(
     <div className="personalInfo-container">
-      <h1>Personal Info</h1>
-      <p>Please provide your name, email address, and phone number.</p>
+      <div className="title-info">
+        <h1>Personal Info</h1>
+        <p>Please provide your name, email address, and phone number.</p>
+      </div>
+
+      <div className="name-box">
+        <label htmlFor="name">Name</label>
+        <input type="text" placeholder='e.g. Stephen King'/>
+      </div>
+      
+      <div className="email-box">
+        <label htmlFor="name">Email Address</label>
+        <input type="text" placeholder='e.g. stephenking@lorem.com'/>
+      </div>
+      <div className="phone-box">
+        <label htmlFor="name">Phone Number</label>
+        <input type="text" placeholder='e.g. +1 234567 890'/>
+      </div>
+
     </div>
   )
 }
