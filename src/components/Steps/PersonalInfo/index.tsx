25be716f71017310d075f35ab9c37ce1dc5ee246
Kelita Lamar <kelitasukita@gmail.com>
2023-02-25 22:43:24 +0100

    Create a Personal Info componente

---
+++ src/components/Steps/PersonalInfo/index.tsx
@@ -0,0 +1,10 @@
+import './index.scss';
+
+export default function personalInfo() {
+  return(
+    <div className="personalInfo-container">
+      <h1>Personal Info</h1>
+      <p>Please provide your name, email address, and phone number.</p>
+    </div>
+  )
+}
