583a1db22a3af03dd0ec4650e36135162fb0e247
Kelita Lamar <kelitasukita@gmail.com>
2023-03-02 00:59:38 +0100

    Call Summary component

--- src/components/StepsMenu.tsx
+++ src/components/StepsMenu.tsx
@@ -2,6 +2,7 @@ import MenuItem from './MenuItem';
 import PersonalInfo from './Steps/PersonalInfo/index';
 import SelectPlan from './Steps/SelectPlan';
 import AddOns from './Steps/Add-ONS';
+import Summary from './Steps/Summary';
 
 import './StepsMenu.scss';
 
@@ -12,14 +13,15 @@ export default function StepsMenu() {
         <div className="menu-box">
           <MenuItem stepNumberBall="1" stepNumberTitle="STEP 1" title="YOUR INFO" selected="" />
           <MenuItem stepNumberBall="2" stepNumberTitle="STEP 2" title="SELECT PLAN" selected="" />
-          <MenuItem stepNumberBall="3" stepNumberTitle="STEP 3" title="ADD-ONS" selected="selected" />
-          <MenuItem stepNumberBall="4" stepNumberTitle="STEP 4" title="SUMMARY" selected="" />
+          <MenuItem stepNumberBall="3" stepNumberTitle="STEP 3" title="ADD-ONS" selected="" />
+          <MenuItem stepNumberBall="4" stepNumberTitle="STEP 4" title="SUMMARY" selected="selected" />
         </div>
 
         <div className="form-container">
           {/* <PersonalInfo /> */}
           {/* <SelectPlan /> */}
-          <AddOns />
+          {/* <AddOns /> */}
+          <Summary />
         </div>
       </div>
 
