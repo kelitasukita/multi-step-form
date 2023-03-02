dfa0f1ce6195e4914eef38c79b46aa86564c800f
Kelita Lamar <kelitasukita@gmail.com>
2023-03-02 21:57:01 +0100

    Import Menu component and fix styles

--- src/components/StepsMenu.tsx
+++ src/components/StepsMenu.tsx
@@ -1,21 +1,17 @@
-import MenuItem from './MenuItem';
+import MenuItem from './MenuItem/MenuItem';
 import PersonalInfo from './Steps/PersonalInfo/PersonalInfo';
 import SelectPlan from './Steps/SelectPlan/SelectPlan';
 import AddOns from './Steps/Add-ONS/Add-ons';
 import Summary from './Steps/Summary/Summary';
 
 import './StepsMenu.scss';
+import Menu from './Menu/Menu';
 
 export default function StepsMenu() {
   return (
     <div className="container">
       <div className="menu-box-container">
-        <div className="menu-box">
-          <MenuItem stepNumberBall="1" stepNumberTitle="STEP 1" title="YOUR INFO" selected="" />
-          <MenuItem stepNumberBall="2" stepNumberTitle="STEP 2" title="SELECT PLAN" selected="" />
-          <MenuItem stepNumberBall="3" stepNumberTitle="STEP 3" title="ADD-ONS" selected="" />
-          <MenuItem stepNumberBall="4" stepNumberTitle="STEP 4" title="SUMMARY" selected="selected" />
-        </div>
+        <Menu />
 
         <div className="form-container">
           {/* <PersonalInfo /> */}
