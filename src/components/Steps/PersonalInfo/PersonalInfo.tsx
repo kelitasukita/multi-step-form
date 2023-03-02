527fa4c4cac8e33f57858c8a8e99f4658cbf6c24
Kelita Lamar <kelitasukita@gmail.com>
2023-03-02 22:43:38 +0100

    Code refactor to component and styles

--- src/components/Steps/PersonalInfo/PersonalInfo.tsx
+++ src/components/Steps/PersonalInfo/PersonalInfo.tsx
@@ -1,29 +1,37 @@
+import Menu from '../../Menu/Menu';
 import './styles.scss';
 
 export default function personalInfo() {
   return (
-    <div className="personalInfo-container">
-      <div className="title-info">
-        <h1>Personal Info</h1>
-        <p>Please provide your name, email address, and phone number.</p>
-      </div>
+    <div className="box-container">
+      <Menu />
+      <div className="form-container">
+        <div className="personalInfo-container">
+          <div className="title-info">
+            <h1>Personal Info</h1>
+            <p>Please provide your name, email address, and phone number.</p>
+          </div>
 
-      <div className="name-box">
-        <label htmlFor="name">Name</label>
-        <input type="text" placeholder='e.g. Stephen King' />
-      </div>
+          <div className="name-box">
+            <label htmlFor="name">Name</label>
+            <input type="text" placeholder='e.g. Stephen King' />
+          </div>
 
-      <div className="email-box">
-        <label htmlFor="name">Email Address</label>
-        <input type="text" placeholder='e.g. stephenking@lorem.com' />
-      </div>
+          <div className="email-box">
+            <label htmlFor="name">Email Address</label>
+            <input type="text" placeholder='e.g. stephenking@lorem.com' />
+          </div>
 
-      <div className="phone-box">
-        <label htmlFor="name">Phone Number</label>
-        <input type="text" placeholder='e.g. +1 234567 890' />
-      </div>
+          <div className="phone-box">
+            <label htmlFor="name">Phone Number</label>
+            <input type="text" placeholder='e.g. +1 234567 890' />
+          </div>
 
-      <button type="submit">Next Step</button>
+          <button type="submit">Next Step</button>
+
+        </div>
+
+      </div>
 
     </div>
   )
