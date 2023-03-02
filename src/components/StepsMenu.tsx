bf67aae4c8ca7a894b8787873485c72d1e2fba06
Kelita Lamar <kelitasukita@gmail.com>
2023-03-02 22:44:59 +0100

    Delete file and recfactor another one

--- src/components/StepsMenu.tsx
+++ src/components/StepsMenu.tsx
@@ -4,22 +4,12 @@ import SelectPlan from './Steps/SelectPlan/SelectPlan';
 import AddOns from './Steps/Add-ONS/Add-ons';
 import Summary from './Steps/Summary/Summary';
 
-import './StepsMenu.scss';
 import Menu from './Menu/Menu';
 
 export default function StepsMenu() {
   return (
     <div className="container">
-      <div className="menu-box-container">
-        <Menu />
-
-        <div className="form-container">
-          {/* <PersonalInfo /> */}
-          {/* <SelectPlan /> */}
-          {/* <AddOns /> */}
-          <Summary />
-        </div>
-      </div>
+      <PersonalInfo />
 
     </div>
   );
