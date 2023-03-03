7f2585c0252a2bbc98f61b23d77b80acc97fae15
Kelita Lamar <kelitasukita@gmail.com>
2023-03-03 11:50:30 +0100

    Set route files to navigation

--- src/components/routes/Routes.tsx
+++ src/components/routes/Routes.tsx
@@ -1,7 +1,5 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  Route,
 } from "react-router-dom";
 
 import PersonalInfo from "../Steps/PersonalInfo/PersonalInfo";
@@ -9,14 +7,23 @@ import SelectPlan from "../Steps/SelectPlan/SelectPlan";
 import AddOns from "../Steps/Add-ONS/Add-ons";
 import Summary from "../Steps/Summary/Summary";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<PersonalInfo />}>
-      <Route path="/select-plan" element={<SelectPlan />} />
-      <Route path="/add-ons" element={<AddOns />} />
-      <Route path="/summary" element={<Summary />} />
-    </Route>
-  )
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PersonalInfo />
+  },
+  {
+    path: "/select-plan",
+    element: <SelectPlan />
+  },
+  {
+    path: "/add-ons",
+    element: <AddOns />
+  },
+  {
+    path: "/summary",
+    element: <Summary />
+  }
+])
 
 export default router;
