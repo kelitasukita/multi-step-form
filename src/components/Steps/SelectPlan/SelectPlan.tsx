2c546e240b6f2e535ffdf9f29a90d0df18ee2414
Kelita Lamar <kelitasukita@gmail.com>
2023-03-03 11:49:50 +0100

    Fix styles and add Menu component

--- src/components/Steps/SelectPlan/SelectPlan.tsx
+++ src/components/Steps/SelectPlan/SelectPlan.tsx
@@ -1,59 +1,66 @@
+import Menu from '../../Menu/Menu';
 import './styles.scss';
 
 export default function SelectPlan() {
   return (
-    <div className="plan-container">
-      <div className="plan-title">
-        <h1>Select your plan</h1>
-        <p>You have the option of monthly or yearly billing.</p>
-      </div>
+    <div className="box-container">
+      <Menu />
+      <div className="form-container">
 
-      <div className="option-plans">
-        <div className="arcade-plan">
-          <label htmlFor="arcade">
-            <svg />
-            <div className="plan-price">
-              <p>Arcade</p>
-              <span>$9/mo</span>
+        <div className="plan-container">
+          <div className="plan-title">
+            <h1>Select your plan</h1>
+            <p>You have the option of monthly or yearly billing.</p>
+          </div>
+
+          <div className="option-plans">
+            <div className="arcade-plan">
+              <label htmlFor="arcade">
+                <svg />
+                <div className="plan-price">
+                  <p>Arcade</p>
+                  <span>$9/mo</span>
+                </div>
+              </label>
             </div>
-          </label>
-        </div>
 
-        <div className="advanced-plan">
-          <label htmlFor="advanced">
-            <svg />
-            <div className="plan-price">
-              <p>Advanced</p>
-              <span>$12/mo</span>
+            <div className="advanced-plan">
+              <label htmlFor="advanced">
+                <svg />
+                <div className="plan-price">
+                  <p>Advanced</p>
+                  <span>$12/mo</span>
+                </div>
+              </label>
             </div>
-          </label>
-        </div>
 
-        <div className="pro-plan">
-          <label htmlFor="pro">
-            <svg />
-            <div className="plan-price">
-              <p>Pro</p>
-              <span>$15/mo</span>
+            <div className="pro-plan">
+              <label htmlFor="pro">
+                <svg />
+                <div className="plan-price">
+                  <p>Pro</p>
+                  <span>$15/mo</span>
+                </div>
+              </label>
             </div>
-          </label>
-        </div>
-      </div>
+          </div>
 
-      <div className="month-year">
-        <div className="better-together">
-          <span>Monthly</span>
-          <label className="switch">
-            <input type="checkbox" />
-            <strong className="slider round" />
-          </label>
-          <span>Yearly</span>
-        </div>
-      </div>
+          <div className="month-year">
+            <div className="better-together">
+              <span>Monthly</span>
+              <label className="switch">
+                <input type="checkbox" />
+                <strong className="slider round" />
+              </label>
+              <span>Yearly</span>
+            </div>
+          </div>
 
-      <div className="back-next">
-        <span>Go back</span>
-        <button type="submit">Next Step</button>
+          <div className="back-next">
+            <span>Go back</span>
+            <button type="submit">Next Step</button>
+          </div>
+        </div>
       </div>
     </div>
   )
