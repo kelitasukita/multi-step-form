3bb87190631cc6178e9292aa5a81d5fca1d83b5f
Kelita Lamar <kelitasukita@gmail.com>
2023-03-07 10:59:57 +0100

    Set an object formPlan on localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,38 @@ import { RouterProvider } from "react-router-dom";
 import router from "./components/routes/Routes";
 
 function App() {
+
+  const formPlan = {
+    monthly: {
+      plans: {
+        arcade: 9,
+        advanced: 12,
+        pro: 15
+      },
+
+      addOns: {
+        online: 1,
+        larger: 2,
+        customizable: 2
+      }
+    },
+    yearly: {
+      plans: {
+        arcade: 90,
+        advanced: 120,
+        pro: 150
+      },
+
+      addOns: {
+        online: 10,
+        larger: 20,
+        customizable: 20
+      }
+    }
+  }
+
+  localStorage.setItem('formPlan', JSON.stringify(formPlan));
+
   return (
     <RouterProvider router={router} />
 
