02a8395cfe95714306aab9f818de0fbe19b39dde
Kelita Lamar <kelitasukita@gmail.com>
2023-03-12 23:13:32 +0100

    Create the mobile menu component and apply styles

---
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -0,0 +1,36 @@
+import "./styles.scss";
+
+import MenuItem from "../MenuItem/MenuItem";
+
+import mobileSvg from "../../assets/images/bg-sidebar-mobile.svg";
+
+export default function MobileMenu() {
+  return (
+    <>
+      <div className="mobile-menu-bg-image">
+        <img src={mobileSvg} alt="" />
+      </div>
+      <div className="mobile-menu-step-ball">
+        <MenuItem
+          stepNumberBall="1"
+          selected={location.pathname === '/'}
+        />
+        <MenuItem
+          stepNumberBall="2"
+          selected={location.pathname === '/select-plan'}
+        />
+        <MenuItem
+          stepNumberBall="3"
+          selected={location.pathname === '/add-ons'}
+        />
+        <MenuItem
+          stepNumberBall="4"
+          selected={location.pathname === '/summary'}
+        />
+      </div>
+
+    </>
+
+  )
+
+}
