f2e4ca5bf8971cf81d7a5fb440bd41e84b5e24ef
Kelita Lamar <kelitasukita@gmail.com>
2023-03-14 12:41:17 +0100

    Call MenuResponsive and apply responsive styles

--- src/components/Steps/Finish/Finish.tsx
+++ src/components/Steps/Finish/Finish.tsx
@@ -3,6 +3,7 @@ import './styles.scss';
 import Menu from "../../Menu/Menu";
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import MobileMenu from '../../MobileMenu/MobileMenu';
 
 
 export default function Finish() {
@@ -26,19 +27,22 @@ export default function Finish() {
   }, [])
 
   return (
-    <div className="box-container">
-      <Menu />
-      <div className="form-container">
-        <div className="thanks-box">
-          <svg />
-          <span>Thank you!</span>
-          <p>Thanks for confirming your subscription!
-            We hope you have fun using our platform.
-            If you ever need support, please feel free to email us at
-            support@loremgaming.com.
-          </p>
+    <>
+      <MobileMenu />
+      <div className="box-container">
+        <Menu />
+        <div className="form-container">
+          <div className="thanks-box">
+            <svg />
+            <span>Thank you!</span>
+            <p>Thanks for confirming your subscription!
+              We hope you have fun using our platform.
+              If you ever need support, please feel free to email us at
+              support@loremgaming.com.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
